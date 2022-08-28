c3e3f415d314198f83b32cc5b1f2d616a84fa834
Gaoustcer <2337284889@qq.com>
2022-08-28 17:02:37 +0800

    commit data generate chaneg reduce the influence of Noise

--- Data/generate_data.py
+++ Data/generate_data.py
@@ -6,7 +6,7 @@ N = 16
 Number_data = 1024 + 32
 X = np.random.random((Number_data,N))
 w = np.random.random((N,1))
-noise = norm.sample((Number_data,1))
+noise = norm.sample((Number_data,1))/Noise
 y = X.dot(w) + noise.numpy()
 np.save('feature.npy',X)
 np.save('label.npy',y)
